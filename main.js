300ec01c75fd7725a321c0c5c61c88956437d066
Matt Cerillo <mcerillo14@gmail.com>
2023-01-30 16:16:01 -0500

    Final

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let count = 0;
+function counterFunc(){
+    count++;
+    document.getElementById("count").innerHTML = count;
+   }
+
+
+function resetFunc(){
+    count=0;
+    document.getElementById("count").innerHTML = count;
+   }
